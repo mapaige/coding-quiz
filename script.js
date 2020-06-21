2f825407cc33337c74a990a162d7070e312baf33
Marcia Paige <mapaige7@gmail.com>
2020-06-20 23:04:15 -0400

    Created new javascript functions

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var questions = [
   { q: "var in Javascript means",c:["coding","variable","expressions","argument"] ,a: "variable" },
   { q: "Which one is not a data type", c:["number","Beelean","String","Null"], a: "Beelean" },
   { q: "3. i++ is:", c:[" Name short for C++","increment","decrement","argruments"] , a:"increment"},
-  { q: "An array", c:["is a collection of values", "index starts at 0, in most cases","all of them.","can have mixed values"], a: "all of them" }
+  { q: "An array:", c:["is a collection of values", "index starts at 0, in most cases","all of them.","can have mixed values"], a: "all of them" },
   
 ];
 var index = 0
@@ -18,15 +18,15 @@ function setTime() {
     var choices = questions[index].c
     document.getElementById("choices").textContent=""
     for (var i=0;i < choices.length;i++){
-      //
+   
       
-                 var li=document.createElement("li")
-                var rowNumber=i+ 1
-                 li.textContent=rowNumber +". " +  choices[i]
-                 li.addEventListener("click",function(){
-                   index++
-                 })
-                 document.getElementById("choices").appendChild(li)
+    var li=document.createElement("li")
+    var rowNumber=i+ 1
+    li.textContent=rowNumber +". " +  choices[i]
+    li.addEventListener("click",function(){
+    index++
+    })
+    document.getElementById("choices").appendChild(li)
 
     }
     if(secondsLeft === 0) {
@@ -47,14 +47,6 @@ function setTime() {
  }
 
 
- function getNextQuestion() {
-
- }
-
- function selectAnswer () {
-
- }
-
  
 
 //   var score = 0; 
@@ -62,12 +54,12 @@ function setTime() {
  
 //   if ((answer === true && questions[i].a === "t") ||  (answer === false && questions[i].a === "f")) {
 //     score++;
-//     alert("Correct!");
-//   }
-//   else {
-//     alert("Wrong!");
-//   }
+//      alert("Correct!");
+//    }
+//    else {
+//      alert("Wrong!");
+//    }
 
-// alert("You got " + score + "/" + questions.length);
+//  alert("You got " + score + "/" + questions.length);
 
  
